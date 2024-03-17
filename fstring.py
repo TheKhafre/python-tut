7c4cc0815e3fb3758dfca36a802b81bc8fbe941c
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-03-17 11:37:32 +0100

    updated the lesson files

---
+++ fstring.py
@@ -0,0 +1,3 @@
+name = input("please enter your name: ").strip().upper()
+
+print(f"hello {name}, \n\tit's time to learn new thing in python today")
---
+++ numbers.py
@@ -0,0 +1,10 @@
+# When you’re writing long numbers
+# you can group digits using underscores 
+# to make large numbers more readable
+
+# this is however only available from python 3.6
+salary = 12_000_000_470
+# the arrangement does not matter the undersore will always be ignored
+group_discount = 10_00_000
+print(salary)
+print(group_discount)
