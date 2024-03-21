0723f2b939f795f7a4e716506d8ea3f5643bb9c2
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-03-21 09:28:43 +0100

    started a lesson from the dictionary section

---
+++ alien_dict.py
@@ -0,0 +1,21 @@
+alien_0 = {
+    "x_pos": 0, 
+    "y_pos": 25, 
+    "speed": "medium"
+}
+print(f"Original position: {alien_0['x_pos']}")
+
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_0['x_pos'] = alien_0['x_pos'] + x_increment
+
+print(f"New position: {alien_0['x_pos']}")
