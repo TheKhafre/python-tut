afc9ed036aa5d21e8aa6265814a9038a04e2f0f8
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-03-21 09:45:59 +0100

    added a dictonary project

---
+++ dict_project.py
@@ -0,0 +1,10 @@
+person_1 = {
+    "first_name": 'John',
+    "last_name": 'Shuaib',
+    "age": 21,
+    "city": 'San Francisco',
+}
+
+for key, value in person_1.items():
+    print(f"\n{key}:")
+    print(f"\t{value}")
