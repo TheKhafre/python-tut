534dee3c7fcd4dcf5c6806a4016bc2c93d212642
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-03-26 19:13:05 +0100

    added an extension to the file wordcountfunc

---
+++ errorHandling/wordcountextend.py
@@ -0,0 +1,16 @@
+def wordCount(filename):
+    try:
+        with open(filename) as f:
+            toCount = f.read()
+    except FileNotFoundError:
+        print(f"the file '{filename[:-4]}' does not exist")
+    else:
+        splitted = toCount.split()
+        counted = len(splitted)
+
+        print(f"there are {counted}-words in {filename[:-4]}")
+
+books = ["pythonpath.txt", "my_text.txt"]
+
+for book in books:
+    wordCount(book)
