fd0a5bbb5e0fcb533bfbfacac36a545d4fa8a7f2
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-03-26 19:51:46 +0100

    turned the json file to a function

--- JSON/smallprogram.py
+++ JSON/smallprogram.py
@@ -1,15 +1,19 @@
 import json
 
-filename = "new_user.json"
+def hello_user():
+    filename = "new_user.json"
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
-    username = input("what is your name: ").strip()
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("what is your name: ").strip()
 
-    with open(filename, "w") as f:
-        json.dump(username, f)
-        print(f"your username has been saved as '{username}'")
-else:
-    print(f"hello {username}, it's nice to have you back!")
+        with open(filename, "w") as f:
+            json.dump(username, f)
+            print(f"your username has been saved as '{username}'")
+    else:
+        print(f"hello {username}, it's nice to have you back!")
+
+
+hello_user()
