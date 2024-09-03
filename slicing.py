e180613e81eb8ef750661b5cb58074aa90daff03
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-09-03 13:38:12 +0100

    added copy list copy update

--- slicing.py
+++ slicing.py
@@ -7,8 +7,25 @@
 print(players[1:3]) """
 
 # looping through a list
-players = ["daniel", "feyi", "fola", "kemi", "joseph"]
+""" players = ["daniel", "feyi", "fola", "kemi", "joseph"]
 
 print("the last 3 names are:")
 for player in players[-3:]:
-    print(player.title())
+    print(player.title()) """
+
+# Copying List
+players = ["Fola", "Kemi", "Joseph"]
+
+other_players = players[:]
+other_players.append("Khafre")
+
+print(players)
+print(other_players)
+
+print("my players are:")
+for player in players:
+    print(player)
+
+print("my friend's players are:")
+for player in other_players:
+    print(player)
