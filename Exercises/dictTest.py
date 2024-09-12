3aa358acd643e8e82182836ff3c5856c851e1e75
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-09-12 19:01:15 +0100

    updated the exercises with dictionary

---
+++ Exercises/dictTest.py
@@ -0,0 +1,13 @@
+test_lone = {
+    'name': 'Lone',
+    'age': 22,
+}
+
+for key,value in test_lone.items():
+    print(f"{key}: {value}")
+
+""" for value in test_lone.values():
+    print(value)
+
+for key in test_lone.keys():
+    print(key) """
