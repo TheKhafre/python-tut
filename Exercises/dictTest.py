e71a032b807d31e212f94b3344e52a18d1603cec
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-09-12 21:12:16 +0100

    tested the dictionary get method

--- Exercises/dictTest.py
+++ Exercises/dictTest.py
@@ -2,9 +2,9 @@ test_lone = {
     'name': 'Lone',
     'age': 22,
 }
-
-for key,value in test_lone.items():
-    print(f"{key}: {value}")
+print(test_lone.get('bade', 'not found'))
+""" for key,value in test_lone.items():
+    print(f"{key}: {value}") """
 
 """ for value in test_lone.values():
     print(value)
