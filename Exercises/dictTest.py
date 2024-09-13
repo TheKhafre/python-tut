fd5cab75e7df34eff0fea138ef1f94e5617fd857
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-09-13 10:08:08 +0100

    added new correction to the dictionary lesson

--- Exercises/dictTest.py
+++ Exercises/dictTest.py
@@ -1,8 +1,21 @@
-test_lone = {
+dictionary = {
+    'loop' : 'a repetitive process',
+    'conditional' : 'reason to do what needs to be done',
+    'dictionary' : 'a series of key-value pairs',
+    'arrays' : 'series of elements of same data type',
+    'variable' : 'a container that takes the shape of what it has'
+}
+
+print('the dictionary contains:')
+for key, value in dictionary.items():
+    print(f'\t{key}: {value}')
+print('\n')
+
+""" test_lone = {
     'name': 'Lone',
     'age': 22,
 }
-print(test_lone.get('bade', 'not found'))
+print(test_lone.get('bade', 'not found')) """
 """ for key,value in test_lone.items():
     print(f"{key}: {value}") """
 
