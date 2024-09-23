5d01df459b6e619ffad9fec41cc7a72c52c35f90
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-09-23 07:48:29 +0100

    introduced None as a placeholder

--- functions/midName.py
+++ functions/midName.py
@@ -1,6 +1,8 @@
-def fullName(first_name, last_name, middle_name=''):
+# None is a placeholder that evaluates to False
+def fullName(first_name, last_name, middle_name=None):
 
     if middle_name:
+        # This only executes to true if there is a middle name supplied
         print(f"hello {first_name} {middle_name} {last_name}")
     else:
         print("there is no provision for middle name")
