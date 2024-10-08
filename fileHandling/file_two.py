e83112516e9cbe78b2283bed60a5c57c773b09d5
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-10-08 17:42:09 +0100

    updated reading file into memory

--- fileHandling/file_two.py
+++ fileHandling/file_two.py
@@ -4,7 +4,15 @@
 
 file_path = '.\\fileHandling\\testfile.txt'
 
-with open(file_path) as opened_file:
+""" with open(file_path) as opened_file:
     for line in opened_file:
-        print(line.strip())
+        print(line.strip()) """
+
+with open(file_path) as opened_file:
+    # when we use read(), it tokenize by letter but
+    # using readlines() tokenize by lines, saving each line to the assigned variable.
+    lines = opened_file.readlines()
+
+for line in lines:
+    print(line.strip())
     
