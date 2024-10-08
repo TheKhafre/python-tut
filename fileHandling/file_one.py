92ab5810c316a8fdb33723b2f1f99231fb164c94
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-10-08 16:37:28 +0100

    updated the file open docstring

--- fileHandling/file_one.py
+++ fileHandling/file_one.py
@@ -1,7 +1,13 @@
 """
     This opens a new file from the provided path
-    and then print the content of the file
+    and then print the content of the file.
+
+    PS: the file needs to be opeend as something
+        because to read the content, we need something to call.
+
+    in this example, we opened as 'opened_file' and save in the variable 'content'.
 """
+
 with open('.\\fileHandling\\testfile.txt') as opened_file:
     content = opened_file.read()
 
