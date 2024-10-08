bd6aabbc1c77f162ced5648594177b58392ab9ee
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-10-08 09:27:08 +0100

    added docstring

--- fileHandling/file_one.py
+++ fileHandling/file_one.py
@@ -1,3 +1,7 @@
+"""
+    This opens a new file from the provided path
+    and then print the content of the file
+"""
 with open('.\\fileHandling\\testfile.txt') as opened_file:
     content = opened_file.read()
 
