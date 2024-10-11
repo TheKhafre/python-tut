ec3bc2d9488ac0ad65afcdfd940215cc81006730
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-10-11 12:40:44 +0100

    completed the method for word counting

---
+++ Exercises/missingFile.py
@@ -0,0 +1,22 @@
+from fileinput import filename
+
+
+def word_count(filename):
+    try:
+        with open(filename) as file:
+            contents = file.read()
+    except FileNotFoundError as e:
+        print(f"Sorry, the file {filename} does not exist.")
+        print(f"Error: {e}")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"There are {num_words} words in {filename}.")
+
+filename = ['.\\fileHandling\\testfile.txt', '.\\fileHandling\\fencefile.txt', '.\\fileHandling\\file_2.txt', '.\\fileHandling\\file_3.txt']
+for file in filename:
+    """ if FileNotFoundError:
+        with open(file, 'a') as file:
+            file.write(f"{filename} does not exit.") """
+    word_count(file)
+    print()
